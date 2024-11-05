173236098e661ca2771b99f62dc550d54e864b8b
fabiomoraiss <rarinaldo@outlook.com>
2024-11-04 22:09:30 -0300

    terminado o trabalho do darlon na minha base

--- minhaBase.py
+++ minhaBase.py
@@ -18,3 +18,33 @@ df['Deduções  com Pensão Alimentícia Total'] = pd.to_numeric(df['Deduções 
 
 pensao_por_ano = df.groupby('Ano-Calendário')['Deduções  com Pensão Alimentícia Total'].sum()
 st.data_editor(pensao_por_ano)
+
+st.write("pensão por genêro")
+pensao_por_genero = df.groupby(['Ano-Calendário', 'Gênero'])['Deduções  com Pensão Alimentícia Total'].sum().reset_index()
+
+st.data_editor(pensao_por_genero)
+
+st.write("pensão por idade")
+pensao_por_idade = df.groupby(['Ano-Calendário', 'Faixa Etária'])['Deduções  com Pensão Alimentícia Total'].sum().reset_index()
+
+st.data_editor(pensao_por_idade)
+
+
+mais_pensao = pensao_por_idade.loc[pensao_por_idade.groupby('Ano-Calendário')['Deduções  com Pensão Alimentícia Total'].idxmax()]
+
+menos_pensao = pensao_por_idade.loc[pensao_por_idade.groupby('Ano-Calendário')['Deduções  com Pensão Alimentícia Total'].idxmin()]
+
+st.write("faixa etária que mais pagou pensão")
+st.data_editor(mais_pensao)
+
+st.write("faixa etária que menos pagou pensão")
+st.data_editor(menos_pensao)
+
+
+
+pensao_sem_menores = pensao_por_idade[(pensao_por_idade['Faixa Etária'] != 'Até 18 anos') & (pensao_por_idade['Faixa Etária'] != 'Todas')]
+
+menos_pensao_sem_menores = pensao_sem_menores.loc[pensao_sem_menores.groupby('Ano-Calendário')['Deduções  com Pensão Alimentícia Total'].idxmin()]
+
+st.write("faixa etária que menos pagou pensão, excluindo menores")
+st.data_editor(menos_pensao_sem_menores)
